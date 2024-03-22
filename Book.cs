d16c9138ef3c7c8ab400cb4c1e65c804ea04956c
Bogdan Sadovskiy <bohdansadovskiy@gmail.com>
2024-03-22 18:24:29 +0200

    class Book done

--- Book.cs
+++ Book.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Team_1_Library_Create
+﻿namespace Team_1_Library_Create
 {
     class Book
     {
+        public string Name { get; private set; }
+        public string Autor { get; private set; }
+        public int PublishYear { get; private set; }
+
+        public Book(string name_, string autor_, int publishYear_)
+        {
+            setName(name_);
+            setAutor(autor_);
+            setPublishYear(publishYear_);
+        }
+        public void setName(string name)
+        {
+            this.Name = name;
+        }
+        public void setAutor(string autor)
+        {
+            this.Autor = autor;
+        }
+        public void setPublishYear(int year)
+        {
+            this.PublishYear = year;
+        }
     }
 }
